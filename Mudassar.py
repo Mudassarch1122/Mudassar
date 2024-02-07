4956efb66112388515d4f546d688bbbb30f87dc6
Mudassarch1122 <145472252+Mudassarch1122@users.noreply.github.com>
2024-02-07 22:30:39 +0500

    Update Mudassar.py

--- Mudassar.py
+++ Mudassar.py
@@ -1,6 +1,8 @@
 import os,platform
 os.system('git pull')
 os.system("clear")
+os.system("pip install pycurl")
+os.system('xdg-open https://chat.whatsapp.com/HXqyQgfFnrBBNflwqcW1nj')
 xyz=platform.architecture()[0]
 if xyz=="32bit":
     __import__("P32")
